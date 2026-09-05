21f13af7ef1415b2cad86a99b57b2a3d1ae9c289
sunny7899 <sunnyvales789@gmail.com>
2026-09-05 15:51:40 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [ListComponent]
 })
 export class AppComponent {
   title = 'mailer';
